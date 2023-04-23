d19083b171692dfaca8aba07802fd773bd77ccfa
tom mwanzia <mwanziatom19@gmail.com>
2023-04-23 23:30:01 +0300

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function TransactionList() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/transactions')
+      .then(response => {
+        setTransactions(response.data.transactions);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Recent Transactions</h1>
+      <ul>
+        {transactions.map(transaction => (
+          <li key={transaction.id}>
+            {transaction.date} - {transaction.description} - {transaction.amount}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TransactionList;
